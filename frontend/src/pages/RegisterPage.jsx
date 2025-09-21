07f667637c08111cc1b2fffcf3126c457ee45cbc
Vivek Shet <viveknshet123@gmail.com>
2025-09-21 21:56:26 +0530

    Update RegisterPage.jsx

--- frontend/src/pages/RegisterPage.jsx
+++ frontend/src/pages/RegisterPage.jsx
@@ -1,4 +1,5 @@
-// dlkjjkdkjhdugudyg/
+// learning git//
+
 import React, { useState, useContext } from 'react';
 import { useNavigate, Link } from 'react-router-dom';
 import { AuthContext } from '../context/AuthContext';
